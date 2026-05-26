1f3e381cc94d5c83c65853598aabd891b0ff005d
Mike Silva Peixoto <rm99879@estudante.fieb.edu.br>
2026-05-26 12:11:00 -0300

    Criado ambiente de DEV

---
+++ src/main/java/com/itb/inf2gm/pizzariadaaura/PizzariadaauraApplication.java
@@ -0,0 +1,15 @@
+package com.itb.inf2gm.pizzariadaaura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariadaauraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariadaauraApplication.class, args);
+
+        System.out.println("Pizzaria da aura é a melhor!");
+	}
+
+}
---
+++ src/test/java/com/itb/inf2gm/pizzariadaaura/PizzariadaauraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2gm.pizzariadaaura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariadaauraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
